ec2b7880324269793cc894af6efdace503c3657d
benson <benson@bensondeMacBook-Pro.local>
2017-08-04 09:59:31 +0800

    recover

--- db/mysql.py
+++ db/mysql.py
@@ -1,7 +1,7 @@
 # _*_ coding:utf-8_*_
 import pymysql.cursors
 
-config = {
+local_config = {
           'host':'127.0.0.1',
           'port':3306,
           'user':'db_admin',
@@ -11,5 +11,14 @@ config = {
           'cursorclass':pymysql.cursors.DictCursor,
           }
 
+dev_config = {
+          'host':'127.0.0.1',
+          'port':3316,
+          'user':'db_admin',
+          'password':'db_admin2015',
+          'db':'s2s',
+          'charset':'utf8mb4',
+          'cursorclass':pymysql.cursors.DictCursor,
+          }
 # Connect to the database
-connection = pymysql.connect(**config)
+connection = pymysql.connect(**dev_config)
