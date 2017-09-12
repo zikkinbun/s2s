a1cd99190e148577501d3e53c75e4b9d2ae41337
benson <jbzhi@uco2.com>
2017-09-12 23:05:59 +0800

    update task

--- tasks.py
+++ tasks.py
@@ -30,50 +30,5 @@ def getOffer():
     t = Thread(target=adxmi.getAdxmiOffer, args=('294daae457e8e335', 100))
     t.start()
 
-def getValidClick():
-    pass
-
-def getRule():
-    sp_rule = SpecailRule()
-    value = sp_rule.getRule(2)
-    print value
-
-def tranoffer():
-    adt = AdvertiseTransOffer()
-    data = adt.getRuleAdvertise(2)
-    print data
-
-def sqltest():
-    line = '{get_price} >= 1'
-    params = re.split(' ', line)
-    table = 'advertise'
-    fields = ['ad_id', 'ad_name', 'pkg_name', 'region', 'category', 'icon_url', 'preview_url', 'get_price', 'payout_type', 'os', 'os_version', 'creatives', 'description', 'status']
-
-    items = ['SELECT ']
-    if not fields:
-        items.append('*')
-    else:
-        for field in fields:
-            items.append(field+',')
-    sql_str = ''.join(items)
-    sql_str = sql_str[0:len(sql_str)-1] + " FROM " + table
-
-    condition_str = ''
-    condition_list = [' WHERE get_price']
-    condition_list.append(params[1])
-    condition_list.append(params[2])
-    condition_str = ''.join(condition_list)
-
-    sql = sql_str + condition_str
-    print sql
-
-def resub():
-    text = 'http://ibrainer.net/cy/100/iPhone7_cy/?referrer ​=1​866'
-    text1 = 'Hot & Sweet" Korean Drama(Vodacom)"'
-
-    data = text.replace(' ', '').replace('%', '')
-    # data1 = text1.replace('"', '')
-    print data
-
-if __name__ == '__main__':
-    resub()
+# if __name__ == '__main__':
+#     resub()
